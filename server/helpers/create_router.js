bcbcdb384f623ee9578cd291141e536844b2eb30
Joanna Gora <joanna.gora@protonmail.com>
2018-09-19 11:53:30 +0100

    add index route, tested with insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,7 +5,12 @@ const createRouter = function(collection) {
   const router = express.Router();
 
   router.get('/', (req, res) => {
-    res.send('fgjsdhjsdfhjksdfkdfgdsf')
+    collection
+      .find()
+      .toArray()
+      .then((docs) => {
+        res.json(docs);
+      })
   })
   return router;
 }
