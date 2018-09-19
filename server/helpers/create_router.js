c65789609121e4b636a4155d69b85cf927d0d337
Joanna Gora <joanna.gora@protonmail.com>
2018-09-19 12:20:24 +0100

    add delete path, tested in insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -29,6 +29,15 @@ const createRouter = function(collection) {
       .toArray())
       .then((docs) => res.json(docs));
   })
+
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne({_id: ObjectID(id)})
+    .then(() => collection.find()
+    .toArray())
+    .then((docs) => res.json(docs));
+  })
   return router;
 }
 
